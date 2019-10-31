90b229cc71fa938b252d99cc6eb078df182b6627
Jono Warren <jono@gener8ads.com>
2019-10-31 13:55:10 +0000

    Random string helpers

---
+++ pkg/random/string.go
@@ -0,0 +1,27 @@
+package random
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+// StringWithCharset creates a random string using a provided charset
+// Note that this IS NOT suitable for crypto applications
+func StringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+// String creates a random string from a standard charset
+// Note that this IS NOT suitable for crypto applications
+func String(length int) string {
+	return StringWithCharset(length, charset)
+}
