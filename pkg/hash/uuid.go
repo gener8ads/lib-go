162ade4fbf52c2fedb577d12f47e27c5d8da1c80
Duncan Allan <duncan@gener8ads.com>
2020-01-30 12:13:49 +0000

    Corrected hash.UUID & bubble up error msg

--- pkg/hash/uuid.go
+++ pkg/hash/uuid.go
@@ -3,9 +3,12 @@ package hash
 import "github.com/google/uuid"
 
 // UUID from namespace + id
-func UUID(source string, salt string) string {
-	namespace, _ := uuid.FromBytes([]byte(source))
+func UUID(source string, salt string) (string, error) {
+	namespace, err := uuid.Parse(source)
+	if err != nil {
+		return "", err
+	}
 	id := uuid.NewSHA1(namespace, []byte(salt))
 
-	return id.String()
+	return id.String(), nil
 }
