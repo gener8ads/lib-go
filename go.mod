ddfc88a1066d3dad6258f5929131b52e37344634
Duncan Allan <duncan@gener8ads.com>
2020-07-24 16:27:40 +0100

    Recaptcha as a gin middleware

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/gener8ads/lib-go
 go 1.12
 
 require (
-	cloud.google.com/go/pubsub v1.0.1
+	cloud.google.com/go/pubsub v1.1.0
 	github.com/DataDog/datadog-go v3.4.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.4.0
 	github.com/go-pg/pg/v9 v9.0.0-beta.11
@@ -17,7 +17,8 @@ require (
 	github.com/philhofer/fwd v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.2.0
 	github.com/tinylib/msgp v1.1.1 // indirect
+	gitlab.com/gener8ads/lib-internal v0.0.0-20200619093628-599060b0c8cd
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
-	google.golang.org/grpc v1.21.1
+	google.golang.org/grpc v1.26.0
 	gopkg.in/DataDog/dd-trace-go.v1 v1.19.0
 )
--- pkg/recaptcha/verify.go
+++ pkg/recaptcha/verify.go
@@ -1,23 +1,39 @@
 package recaptcha
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
+	"strconv"
+	"time"
 
+	"github.com/gener8ads/lib-go/pkg/jsonapi"
+	"github.com/gin-gonic/gin"
 	jsoniter "github.com/json-iterator/go"
+	"gitlab.com/gener8ads/lib-internal/pkg/ginutil"
 )
 
 const endpoint = "https://www.google.com/recaptcha/api/siteverify"
+
+// CaptchaScore is a key, against which the score for the captcha is stored in the gin.Context, for later use
+const CaptchaScore = "captcha_score"
+
+const (
+	versionHeader = "X-Captcha-Version"
+	tokenHeader   = "X-Captcha-Token"
+)
 
 var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 // VerifyResponse from recaptcha
 type VerifyResponse struct {
-	Success  bool    `json:"success"`
-	Score    float64 `json:"score"`
-	Action   string  `json:"action"`
-	Hostname string  `json:"hostname"`
+	Success            bool      `json:"success"`
+	Score              float64   `json:"score"`
+	Action             string    `json:"action"`
+	Hostname           string    `json:"hostname"`
+	ChallengeTimestamp time.Time `json:"challenge_ts"`
 }
 
 // Verify a recaptcha token
@@ -43,4 +59,96 @@ func Verify(secret string, token string, ip string) (*VerifyResponse, error) {
 	decodeErr := json.Unmarshal(body, &res)
 
 	return res, decodeErr
+}
+
+// Middleware is the verification process implemented as a GIN middleware function
+func Middleware(expectedAction string) gin.HandlerFunc {
+	expectedAction = fmt.Sprintf("action/%s", expectedAction)
+
+	enabled, _ := strconv.ParseBool(os.Getenv("RECAPTCHA_ENABLED"))
+
+	secrets := map[int]string{
+		2: os.Getenv("RECAPTCHA_V2_SECRET"),
+		3: os.Getenv("RECAPTCHA_V3_SECRET"),
+	}
+
+	return func(c *gin.Context) {
+		if !enabled {
+			return
+		}
+
+		captchaVersion, err := strconv.Atoi(c.GetHeader(versionHeader))
+		if err != nil {
+			jsonapi.Error(c, jsonapi.ErrorResponse{
+				Status: http.StatusUnprocessableEntity,
+				Code:   "recaptcha.missingVersion",
+				Detail: "recaptcha.missingVersion",
+			})
+			return
+		}
+
+		secret, exists := secrets[captchaVersion]
+		if !exists || secret == "" {
+			jsonapi.Error(c, jsonapi.ErrorResponse{
+				Status: http.StatusUnprocessableEntity,
+				Code:   "recaptcha.missingVersion",
+				Detail: "recaptcha.missingVersion",
+			})
+			return
+		}
+
+		res, err := Verify(secret, c.GetHeader(tokenHeader), ginutil.ExtractIP(c))
+		if err != nil || !res.Success {
+			jsonapi.Error(c, jsonapi.ErrorResponse{
+				Status: http.StatusUnprocessableEntity,
+				Code:   "recaptcha.failed",
+				Detail: "recaptcha.failed",
+			})
+			return
+		}
+
+		if captchaVersion == 3 {
+			if res.Action != expectedAction {
+				jsonapi.Error(c, jsonapi.ErrorResponse{
+					Status: http.StatusUnprocessableEntity,
+					Code:   "recaptcha.incorrectAction",
+					Detail: "recaptcha.incorrectAction",
+				})
+				return
+			}
+
+			minScore, _ := strconv.ParseFloat(os.Getenv("RECAPTCHA_MIN_SCORE"), 32)
+			if res.Score < minScore {
+				jsonapi.Error(c, jsonapi.ErrorResponse{
+					Status: http.StatusUnprocessableEntity,
+					Code:   "recaptcha.challenge",
+					Detail: "recaptcha.challenge",
+				})
+				return
+			}
+
+			expectedHostname := os.Getenv("RECAPTCHA_HOSTNAME")
+			if expectedHostname != "" && res.Hostname != expectedHostname {
+				jsonapi.Error(c, jsonapi.ErrorResponse{
+					Status: http.StatusUnprocessableEntity,
+					Code:   "recaptcha.challenge",
+					Detail: "recaptcha.challenge",
+				})
+				return
+			}
+
+			maxTime, _ := strconv.Atoi(os.Getenv("RECAPTCHA_MAX_TIME"))
+			if maxTime > 0 && res.ChallengeTimestamp.Add(time.Duration(maxTime)*time.Second).Before(time.Now()) {
+				jsonapi.Error(c, jsonapi.ErrorResponse{
+					Status: http.StatusUnprocessableEntity,
+					Code:   "recaptcha.challenge",
+					Detail: "recaptcha.challenge",
+				})
+				return
+			}
+
+			// Stash the score in the context in case we need to publish it
+			c.Set(CaptchaScore, res.Score)
+		}
+	}
 }
