823276909bf88df36a2413aa70b8ecdbae742133
Duncan Allan <duncan@gener8ads.com>
2020-10-26 15:37:21 +0000

    use Zap logger for grpc

--- pkg/grpc/grpc.go
+++ pkg/grpc/grpc.go
@@ -4,12 +4,12 @@ import (
 	"time"
 
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
-	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
+	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
 	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
 	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 	grpc_opentracing "github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
 	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
-	"github.com/sirupsen/logrus"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
@@ -18,25 +18,27 @@ import (
 	grpc_datadogtrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/google.golang.org/grpc"
 )
 
+var (
+	zapLogger  *zap.Logger
+	customFunc grpc_zap.CodeToLevel
+)
+
 // NewServer returns an instrumented gRPC server
 func NewServer(serviceName string) *grpc.Server {
-	logrusLogger := logrus.New()
-	// Logrus entry is used, allowing pre-definition of certain fields by the user.
-	logrusEntry := logrus.NewEntry(logrusLogger)
+	zapLogger = zap.NewExample()
 	// Shared options for the logger, with a custom gRPC code to log level function.
-	opts := []grpc_logrus.Option{
-		grpc_logrus.WithLevels(grpc_logrus.DefaultCodeToLevel),
+	opts := []grpc_zap.Option{
+		grpc_zap.WithLevels(customFunc),
 	}
-
-	// Make sure that log statements internal to gRPC library are logged using the logrus Logger as well.
-	grpc_logrus.ReplaceGrpcLogger(logrusEntry)
+	// Make sure that log statements internal to gRPC library are logged using the zapLogger as well.
+	grpc_zap.ReplaceGrpcLogger(zapLogger)
 
 	server := grpc.NewServer(
 		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
 			grpc_ctxtags.StreamServerInterceptor(grpc_ctxtags.WithFieldExtractor(grpc_ctxtags.CodeGenRequestFieldExtractor)),
 			grpc_opentracing.StreamServerInterceptor(),
 			grpc_prometheus.StreamServerInterceptor,
-			grpc_logrus.StreamServerInterceptor(logrusEntry, opts...),
+			grpc_zap.StreamServerInterceptor(zapLogger, opts...),
 			grpc_recovery.StreamServerInterceptor(),
 		)),
 		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
@@ -44,7 +46,7 @@ func NewServer(serviceName string) *grpc.Server {
 			grpc_opentracing.UnaryServerInterceptor(),
 			grpc_prometheus.UnaryServerInterceptor,
 			grpc_datadogtrace.UnaryServerInterceptor(grpc_datadogtrace.WithServiceName(serviceName)),
-			grpc_logrus.UnaryServerInterceptor(logrusEntry, opts...),
+			grpc_zap.UnaryServerInterceptor(zapLogger, opts...),
 			grpc_recovery.UnaryServerInterceptor(),
 		)),
 		grpc.KeepaliveParams(keepalive.ServerParameters{
