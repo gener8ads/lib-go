f6a486ea4c9c4569c76a53aa62da386fbd7d72ec
DuncanGener8 <60470931+DuncanGener8@users.noreply.github.com>
2021-05-12 14:41:03 +0100

    (GEN-3380) Pubsub logging middleware (#14)
    PubSub Logging middleware
    Using an older version of pubsub library

--- go.mod
+++ go.mod
@@ -3,16 +3,15 @@ module github.com/gener8ads/lib-go
 go 1.12
 
 require (
-	cloud.google.com/go v0.45.1
-	cloud.google.com/go/pubsub v1.0.1
+	cloud.google.com/go/pubsub v1.3.1
+	cloud.google.com/go/storage v1.10.0
 	github.com/DataDog/datadog-go v3.4.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.4.0
-	github.com/go-pg/pg/extra/pgotel v0.1.0
 	github.com/go-pg/pg/v10 v10.6.2
 	github.com/gogo/protobuf v1.2.0 // indirect
-	github.com/google/go-cmp v0.5.3 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/google/jsonapi v0.0.0-20181016150055-d0428f63eb51
-	github.com/google/uuid v1.1.1
+	github.com/google/uuid v1.1.2
 	github.com/grpc-ecosystem/go-grpc-middleware v1.0.0
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
 	github.com/hashicorp/go-version v1.2.1
@@ -20,12 +19,16 @@ require (
 	github.com/opentracing/opentracing-go v1.1.0 // indirect
 	github.com/philhofer/fwd v1.0.0 // indirect
 	github.com/prometheus/client_golang v0.9.3-0.20190127221311-3c4408c8b829 // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
 	github.com/tinylib/msgp v1.1.1 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.0.0 // indirect
 	go.uber.org/zap v1.16.0
 	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9 // indirect
-	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
-	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
-	google.golang.org/grpc v1.27.0
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/net v0.0.0-20210420210106-798c2154c571 // indirect
+	golang.org/x/sys v0.0.0-20210420205809-ac73e9fd8988 // indirect
+	google.golang.org/api v0.45.0 // indirect
+	google.golang.org/genproto v0.0.0-20210420162539-3c870d7478d2 // indirect
+	google.golang.org/grpc v1.37.0
 	gopkg.in/DataDog/dd-trace-go.v1 v1.19.0
 )
---
+++ pkg/pubsub/logging.go
@@ -0,0 +1,132 @@
+package pubsub
+
+import (
+	"context"
+	"os"
+	"strings"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+
+	"cloud.google.com/go/pubsub"
+)
+
+const (
+	ErrorKey  = "pubsub.error"
+	ResultKey = "pubsub.result"
+)
+
+type options struct {
+	messageFunc MessageProducer
+}
+
+// To prevent constant re-initialisation, let's trap options & the logger in some global vars
+var pubsubOptions *options
+var pubsubLogger *zap.Logger
+
+// HandlerFunc is a pubsub handler function which can be used in pubsub subscription receive.
+type HandlerFunc func(ctx context.Context, m *pubsub.Message)
+
+func defaultOptions() *options {
+	return &options{
+		messageFunc: defaultMessageProducer,
+	}
+}
+
+// Option describes a logging option.
+type Option func(*options)
+
+// WithMessageProducer overrides the default message producer for producing log messages.
+func WithMessageProducer(messageFunc MessageProducer) Option {
+	return func(o *options) {
+		o.messageFunc = messageFunc
+	}
+}
+
+// MessageProducer produces log messages.
+type MessageProducer func(ctx context.Context, msg string, logger *zap.Logger, fields []zapcore.Field)
+
+// defaultMessageProducer writes default log messages.
+func defaultMessageProducer(ctx context.Context, msg string, logger *zap.Logger, fields []zapcore.Field) {
+	logger.Info(msg, fields...)
+}
+
+func InitialiseLoggingHandler(opts ...Option) {
+	pubsubOptions = defaultOptions()
+	for _, opt := range opts {
+		opt(pubsubOptions)
+	}
+	// First, define our level-handling logic.
+	highPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.ErrorLevel
+	})
+	lowPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl < zapcore.ErrorLevel
+	})
+
+	config := zapcore.EncoderConfig{
+		MessageKey:     string(ResultKey),
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.MillisDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+
+	// High-priority output should also go to standard error, and low-priority
+	// output should also go to standard out.
+	jsonDebugging := zapcore.Lock(os.Stdout)
+	jsonErrors := zapcore.Lock(os.Stderr)
+	jsonEncoder := zapcore.NewJSONEncoder(config)
+
+	// Join the outputs, encoders, and level-handling functions into
+	// zapcore.Cores, then tee the four cores together.
+	core := zapcore.NewTee(
+		zapcore.NewCore(jsonEncoder, jsonErrors, highPriority),
+		zapcore.NewCore(jsonEncoder, jsonDebugging, lowPriority),
+	)
+
+	pubsubLogger = zap.New(core)
+}
+
+// LoggingHandler returns a logging handler middleware which uses Zap as the logger.
+func LoggingHandler(next HandlerFunc, subscriptionName string) HandlerFunc {
+	return func(ctx context.Context, msg *pubsub.Message) {
+		defer pubsubLogger.Sync()
+		startTime := time.Now()
+
+		if msg.Attributes == nil {
+			msg.Attributes = make(map[string]string, 0)
+		}
+
+		next(ctx, msg)
+
+		fields := []zapcore.Field{
+			zap.String("pubsub.subscription", subscriptionName),
+			zap.String("pubsub.msg.id", msg.ID),
+			zap.Time("pubsub.msg.publishTime", msg.PublishTime),
+			zap.Duration("pubsub.latency", time.Since(startTime)),
+		}
+
+		if msg.DeliveryAttempt != nil {
+			fields = append(fields, zap.Int("pubsub.msg.deliveryAttempt", *msg.DeliveryAttempt))
+		}
+
+		for key, val := range msg.Attributes {
+			if strings.HasPrefix(key, "pubsub.") && key != ResultKey {
+				fields = append(fields, zap.String(key, val))
+			}
+		}
+
+		result := "not provided"
+		if val, ok := msg.Attributes[string(ResultKey)]; ok {
+			result = val
+		}
+
+		if pubsubOptions.messageFunc != nil {
+			pubsubOptions.messageFunc(ctx, result, pubsubLogger, fields)
+		}
+	}
+}
