b55444bb00c47cdb214513d9910157485d3d7f7d
Duncan Allan <duncan@gener8ads.com>
2021-05-12 10:00:12 +0100

    Add the logging back in

--- pkg/pubsub/logging.go
+++ pkg/pubsub/logging.go
@@ -3,6 +3,9 @@ package pubsub
 import (
 	"context"
 	"log"
+	"os"
+	"strings"
+	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -52,42 +55,42 @@ func defaultMessageProducer(ctx context.Context, msg string, logger *zap.Logger,
 
 func InitialiseLoggingHandler(opts ...Option) {
 	log.Printf("In Initialisation routing: %#+v\n", opts)
-	// pubsubOptions = defaultOptions()
-	// for _, opt := range opts {
-	// 	opt(pubsubOptions)
-	// }
-	// // First, define our level-handling logic.
-	// highPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
-	// 	return lvl >= zapcore.ErrorLevel
-	// })
-	// lowPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
-	// 	return lvl < zapcore.ErrorLevel
-	// })
+	pubsubOptions = defaultOptions()
+	for _, opt := range opts {
+		opt(pubsubOptions)
+	}
+	// First, define our level-handling logic.
+	highPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.ErrorLevel
+	})
+	lowPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl < zapcore.ErrorLevel
+	})
 
-	// config := zapcore.EncoderConfig{
-	// 	MessageKey:     string(ResultKey),
-	// 	StacktraceKey:  "stacktrace",
-	// 	LineEnding:     zapcore.DefaultLineEnding,
-	// 	EncodeLevel:    zapcore.LowercaseLevelEncoder,
-	// 	EncodeTime:     zapcore.ISO8601TimeEncoder,
-	// 	EncodeDuration: zapcore.MillisDurationEncoder,
-	// 	EncodeCaller:   zapcore.ShortCallerEncoder,
-	// }
+	config := zapcore.EncoderConfig{
+		MessageKey:     string(ResultKey),
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.MillisDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
 
-	// // High-priority output should also go to standard error, and low-priority
-	// // output should also go to standard out.
-	// jsonDebugging := zapcore.Lock(os.Stdout)
-	// jsonErrors := zapcore.Lock(os.Stderr)
-	// jsonEncoder := zapcore.NewJSONEncoder(config)
+	// High-priority output should also go to standard error, and low-priority
+	// output should also go to standard out.
+	jsonDebugging := zapcore.Lock(os.Stdout)
+	jsonErrors := zapcore.Lock(os.Stderr)
+	jsonEncoder := zapcore.NewJSONEncoder(config)
 
-	// // Join the outputs, encoders, and level-handling functions into
-	// // zapcore.Cores, then tee the four cores together.
-	// core := zapcore.NewTee(
-	// 	zapcore.NewCore(jsonEncoder, jsonErrors, highPriority),
-	// 	zapcore.NewCore(jsonEncoder, jsonDebugging, lowPriority),
-	// )
+	// Join the outputs, encoders, and level-handling functions into
+	// zapcore.Cores, then tee the four cores together.
+	core := zapcore.NewTee(
+		zapcore.NewCore(jsonEncoder, jsonErrors, highPriority),
+		zapcore.NewCore(jsonEncoder, jsonDebugging, lowPriority),
+	)
 
-	// pubsubLogger = zap.New(core)
+	pubsubLogger = zap.New(core)
 }
 
 // LoggingHandler returns a logging handler middleware which uses Zap as the logger.
@@ -95,39 +98,39 @@ func LoggingHandler(next HandlerFunc, subscriptionName string) HandlerFunc {
 	log.Printf("Outside of returned function: %#+v\n", subscriptionName)
 	return func(ctx context.Context, msg *pubsub.Message) {
 		log.Printf("msg: %#+v\n", msg)
-		// defer pubsubLogger.Sync()
-		// startTime := time.Now()
+		defer pubsubLogger.Sync()
+		startTime := time.Now()
 
-		// if msg.Attributes == nil {
-		// 	msg.Attributes = make(map[string]string, 0)
-		// }
+		if msg.Attributes == nil {
+			msg.Attributes = make(map[string]string, 0)
+		}
 
 		next(ctx, msg)
 
-		// fields := []zapcore.Field{
-		// 	zap.String("pubsub.subscription", subscriptionName),
-		// 	zap.String("pubsub.msg.id", msg.ID),
-		// 	zap.Time("pubsub.msg.publishTime", msg.PublishTime),
-		// 	zap.Duration("pubsub.latency", time.Since(startTime)),
-		// }
+		fields := []zapcore.Field{
+			zap.String("pubsub.subscription", subscriptionName),
+			zap.String("pubsub.msg.id", msg.ID),
+			zap.Time("pubsub.msg.publishTime", msg.PublishTime),
+			zap.Duration("pubsub.latency", time.Since(startTime)),
+		}
 
-		// if msg.DeliveryAttempt != nil {
-		// 	fields = append(fields, zap.Int("pubsub.msg.deliveryAttempt", *msg.DeliveryAttempt))
-		// }
+		if msg.DeliveryAttempt != nil {
+			fields = append(fields, zap.Int("pubsub.msg.deliveryAttempt", *msg.DeliveryAttempt))
+		}
 
-		// for key, val := range msg.Attributes {
-		// 	if strings.HasPrefix(key, "pubsub.") && key != ResultKey {
-		// 		fields = append(fields, zap.String(key, val))
-		// 	}
-		// }
+		for key, val := range msg.Attributes {
+			if strings.HasPrefix(key, "pubsub.") && key != ResultKey {
+				fields = append(fields, zap.String(key, val))
+			}
+		}
 
-		// result := "not provided"
-		// if val, ok := msg.Attributes[string(ResultKey)]; ok {
-		// 	result = val
-		// }
+		result := "not provided"
+		if val, ok := msg.Attributes[string(ResultKey)]; ok {
+			result = val
+		}
 
-		// if pubsubOptions.messageFunc != nil {
-		// 	pubsubOptions.messageFunc(ctx, result, pubsubLogger, fields)
-		// }
+		if pubsubOptions.messageFunc != nil {
+			pubsubOptions.messageFunc(ctx, result, pubsubLogger, fields)
+		}
 	}
 }
